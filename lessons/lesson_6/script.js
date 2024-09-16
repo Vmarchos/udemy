1434f025a34daa09a7d20d7b2bcb1211d798c7c7
Valery_Marchenko <Vmarchos19@gmail.com>
2024-09-16 22:22:25 +0300

    hadr

---
+++ lessons/lesson_6/script.js
@@ -0,0 +1,25 @@
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)
+
+//     let result = "";
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+
+//   result += "\n";
+// }
