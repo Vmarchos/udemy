278695fa8fb613e11ab02e979bf367cb96647f10
Valery_Marchenko <Vmarchos19@gmail.com>
2024-09-16 19:06:57 +0300

    leson

--- lessons/leson_3/script.js
+++ lessons/leson_3/script.js
@@ -100,3 +100,42 @@ let num = 50;
 //   }
 //   console.log(i);
 // }
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+  for (let j = 0; j < 3; j++) {
+    console.log(j);
+  }
+}
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = "";
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+  for (let j = 0; j < i; j++) {
+    result += "*";
+  }
+
+  result += "\n";
+}
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`first level:${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`second level:${j}`);
+    for (let k = 0; k < 5; k++) {
+      if (k === 2)
+        break first;
+         console.log(`third level:${k}`);
+    }
+  }
+}
