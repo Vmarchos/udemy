a752bd6e12964221134ccd5d9d11b556bb734595
Valery_Marchenko <Vmarchos19@gmail.com>
2024-09-16 22:03:01 +0300

    itaration

---
+++ lessons/lesson_5/script.js
@@ -0,0 +1,48 @@
+// Место для первой задачи
+function firstTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const arr = [3, 5, 8, 16, 20, 23, 50];
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+  }
+  console.log(result);
+
+  return result;
+}
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, "Shopping", 20, "Homework"];
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] === "number") {
+      data[i] = data[i] * 2;
+    } else if (typeof data[i] === "string") {
+      data[i] = data[i] += " - done";
+    }
+  }
+
+  console.log(data);
+
+  // Не трогаем
+  return data;
+}
+secondTask();
+
+// Место для третьей задачи
+function thirdTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, "Shopping", 20, "Homework"];
+  const result = [];
+  for (let i = 1; i <= data.length; i++) {
+    result[i - 1] = data[data.length - i]
+}
+
+console.log(result);
+
+  // Не трогаем
+  return result;
+}
+thirdTask();
