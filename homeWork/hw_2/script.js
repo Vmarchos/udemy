7095a39e0b3b81bdbda58df99fc7dc0590015c6c
Valery_Marchenko <Vmarchos19@gmail.com>
2024-09-18 15:40:21 +0300

    home_wokr

--- homeWork/hw_2/script.js
+++ homeWork/hw_2/script.js
@@ -20,3 +20,35 @@ function standardizeStrings(arr) {
 
 standardizeStrings(favoriteCities);
 console.log(standardizeStrings(favoriteCities));
+
+const someString = "This is some strange string";
+
+function reverse(str) {
+  if (typeof str !== "string") {
+    return "Ошибка!";
+  }
+  let strMass = [];
+  strMass = str.split("").reverse("").join("");
+  return strMass;
+}
+reverse(someString);
+console.log(reverse(someString));
+
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+
+function availableCurr(arr, missingCurr) {
+  let str = "";
+  arr.length === 0
+    ? (str = "Нет доступных валют")
+    : (str = "Доступные валюты:\n");
+
+  arr.forEach(function (curr) {
+    if (curr !== missingCurr) {
+      str += `${curr}\n`;
+    }
+  });
+  return str;
+}
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY')
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'))
